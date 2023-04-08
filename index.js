7fafb787578eca8403a2da513ae662e5a347a5d6
kellyetue@gmail.com <kellyetue@gmail.com>
2023-04-08 11:48:28 -0700

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return 'This one is on me!' 
+  } else if (someValue >= 400 && someValue <= 2000) {
+      return 'That will be twenty bucks.'  
+  } else if (someValue > 2000 && someValue < 2500) {
+      return 'I will gladly take your thirty bucks.'
+  } else (someValue >= 2500);
+      return 'No can do.'
+}   
+
+function ternaryCheckCity(someCity){
+  return someCity === 'NYC'?'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+  switch(someTip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
